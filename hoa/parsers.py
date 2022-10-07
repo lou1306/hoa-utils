lou1306/hoa-utils

commit 9662e4fa3956d81a981cce315d9d81a570a52aef

Ignore True literals when parsing And expression
(incidentally this fixes a bug when all args are TrueFormula)

hoa/parsers.py
@@ -313,6 +313,9 @@ class HOATransformer(Transformer):
 
     def and_label_expr(self, args):
         """Parse the 'and_label_expr' node."""
+        nottrue_args = [a for a in args if type(a) != TrueFormula]
+        if not nottrue_args:
+            return TrueFormula()
         return reduce(operator.and_, args)
 
     def not_label_expr(self, args):

commit fd5087c44aa7188859c23c5809998640af9eeff4

Fix a bug introduced in 9662e4fa3956d81a981cce315d9d81a570a52aef
We should only reduce by and_ to the arguments that have been filtered

hoa/parsers.py
@@ -316,7 +316,7 @@ class HOATransformer(Transformer):
         nottrue_args = [a for a in args if type(a) != TrueFormula]
         if not nottrue_args:
             return TrueFormula()
-        return reduce(operator.and_, args)
+        return reduce(operator.and_, nottrue_args)
 
     def not_label_expr(self, args):
         """Parse the 'not_label_expr' node."""